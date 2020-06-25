584f2dc3a204edb66c0aa964bc9b2a8fffed61e0
ourbanow <oliviaurb@hotmail.com>
2020-06-25 18:43:24 +0200

    -

--- google_scraping_WIP.R
+++ google_scraping_WIP.R
@@ -1,3 +1,5 @@
+library(httr)
+
 PLAY_STORE_BASE_URL <- "https://play.google.com"
 
 app_id <- "com.bshg.homeconnect.android.release"
@@ -7,3 +9,5 @@ country <- "DE"
 app_url <- paste0(PLAY_STORE_BASE_URL,"/store/apps/details?id=",app_id,"&hl=",lang,"&gl=",country)
 
 reviews_url <- paste0(PLAY_STORE_BASE_URL,"/_/PlayStoreUi/data/batchexecute?hl=",lang,"&gl=",country)
+
+r <- httr::GET(reviews_url)
