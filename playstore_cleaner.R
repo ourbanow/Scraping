4f7f25df572018c9f189a94d1c2c4304c4d28a8c
ourbanow <oliviaurb@hotmail.com>
2020-06-26 11:54:38 +0200

    wrote function

---
+++ playstore_cleaner.R
@@ -0,0 +1,25 @@
+# load google reviews
+
+library(jsonlite)
+library(tidyverse) 
+
+clean_playstore_reviews <- function(reviews_file, app_id, country){
+    rawfile <- fromJSON(txt = reviews_file)
+
+    # Retrieve data
+    review_title <- rawfile$score # There is no title
+    review_date <- as.POSIXct(rawfile$at, format = "datetime.datetime(%Y, %m, %d, %H, %M, %S)")
+    review_star <- rawfile$score
+    review_text <- rawfile$content
+    
+    # Return a tibble
+    my_tibble <- tibble(review_title,
+           review_date,
+           review_star,
+           review_text)
+
+    filename <- paste0(Sys.Date(),"_playstore_",app_id,"_",country,"_",length(my_tibble),"_Reviews.RDS")
+    saveRDS(my_tibble, file = filename)
+    message("Saved file ",filename)
+    } 
+
